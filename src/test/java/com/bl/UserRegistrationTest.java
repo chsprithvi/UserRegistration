638e277b3dc85e09b724892f0b1d990dc3ba1675
chsprithvi <chsprithvi@gmail.com>
2020-09-22 16:01:06 +0530

    [Siddhartha]Modified:changed from first name to last name

--- src/test/java/com/bl/UserRegistrationTest.java
+++ src/test/java/com/bl/UserRegistrationTest.java
@@ -6,19 +6,19 @@ import org.junit.Test;
 public class UserRegistrationTest {
     UserRegistration userRegistration=new UserRegistration();
     @Test
-    public void givenFirstName_whenProper_shouldReturnTrue() {
+    public void givenLastName_whenProper_shouldReturnTrue() {
         boolean result = userRegistration.validateName("Prithvi");
         Assert.assertTrue(result);
     }
 
     @Test
-    public void givenFirstName_whenShort_shouldReturnFalse() {
+    public void givenLastName_whenShort_shouldReturnFalse() {
         boolean result = userRegistration.validateName("OK");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void givenFirstName_whenStartWithSmallLetters_shouldReturnFalse() {
+    public void givenLastName_whenStartWithSmallLetters_shouldReturnFalse() {
         boolean result = userRegistration.validateName("prithvi");
         Assert.assertFalse(result);
     }
